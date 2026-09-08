59e73a0e19fa770a0f73dcb4587324751fde6da6
prabalshakya262-hue <prabalshakya262@gmail.com>
2026-09-08 15:10:59 +0530

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Start Celery worker in background
 celery -A app.celery_app.celery_app worker --loglevel=info --concurrency=2 &
-# Start Uvicorn in foreground
+
+# Start Uvicorn (FastAPI) in foreground
 uvicorn app.main:app --host 0.0.0.0 --port 8000
